bd1bde72a183c9f5a4c7e0f85f4a0edf79e82afb
xineohperif <tvbbd2@gmail.com>
2024-04-29 14:01:41 +0930

    update

--- AddSub.asm
+++ AddSub.asm
@@ -2,3 +2,41 @@
 // (R0, R1, R2, R3 refer to RAM[0], RAM[1], RAM[2], and RAM[3], respectively.)
 
 // Put your code here.
+
+// PSEUDO code
+// x = R1
+// y = R2
+// z = R3
+
+// x = x + y - z
+// R0 = x
+
+@R1
+D=M
+@x
+M=D // x = R1
+
+@R2
+D=M
+@y
+M=D // y = R2
+
+@R3
+D=M
+@z
+M=D // z = R3
+
+@y
+D=M // D = y
+@x
+M=D+M // x = x+y
+
+@z
+D=M // D = z
+@x
+M=M-D // x = x-z
+
+@x
+D=M
+@R0
+M=D
